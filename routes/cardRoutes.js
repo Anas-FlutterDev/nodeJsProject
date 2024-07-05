71602b4f4d9cb447a08e25f3306d3d97f3edc528
AnasRajpoot <anasrajpoot038@gmail.com>
2024-07-05 08:31:57 -0700

    correct mistake

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -7,7 +7,7 @@ import  auth  from '../middlewares/auth.js'; // assuming .mjs extension for ES m
 
 router.post('/', cardController.createCard);
 router.get('/',auth(true), cardController.getAllCards);
-router.gecat('/:id',cardController.getCardById);
+router.get('/:id',cardController.getCardById);
 router.put('/:id', auth(true),rdController.updateCard);
 router.delete('/:id', auth(true), cardController.deleteCard);
 
